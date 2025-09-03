436ce0e5256ac7c3ad11e3794bc93bf1dfdda292
Hrishik Shaji <hrishikminishaji@gmail.com>
2025-09-03 19:35:03 +0530

    sample check

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,7 +5,7 @@ export const dynamic = "force-dynamic"
 
 export default function Home() {
 	return (
-		<div className="h-screen w-full flex flex-col items-center justify-center gap-10">
+		<div className="h-screen bg-gray-200 w-full flex flex-col items-center justify-center gap-10">
 			<CreateProduct />
 			<Products />
 		</div>
