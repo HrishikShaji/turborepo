49bdaccfab8e9f8be91f2aa7c1c5d8210d8f268a
Hrishik Shaji <hrishikminishaji@gmail.com>
2025-09-04 09:35:47 +0530

    added prisma

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,14 @@
+import { prisma, User } from "@repo/db";
 import CreateProduct from "./create-product/create-product";
 import Products from "./products/products";
 
 export const dynamic = "force-dynamic"
 
-export default function Home() {
+export default async function Home() {
+	const user = await prisma.user.findFirst() as User
 	return (
 		<div >
+			{user?.email ?? "No user added yet"}
 			<CreateProduct />
 			<Products />
 		</div>
